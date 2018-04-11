0e6bb8dcc60e3e28f35b92c83e5f49bcb7c9ea17
winit30 <vineetm616@gmail.com>
2018-04-11 17:40:21 +0530

    mongodb added

--- db/db.js
+++ db/db.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
+mongodb_url = "mongodb://vineet30:Buntymis30!@ds141889.mlab.com:41889/modulegenerator";
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/MyApp_DB');
+mongoose.connect(mongodb_url);
 
 module.exports = {mongoose};
